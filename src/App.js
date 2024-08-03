80bf74a1bcee0880f4106600a7e9c6531d28ad62
sina <siyam1990@gmail.com>
2024-08-03 15:58:56 +0330

    use timecontext in app.js add time and date state, add function for set them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, createContext, useContext } from "react";
 import AddFormComponent from "./components/AddFormComponent.js";
 import TaskListComponent from "./components/taskListComponent.js";
 import ButtonComponent from "./components/ButtonComponent.js";
+import { TimeContext } from "./store/time-context.js";
+
 function App() {
+  const [deadLineDate, setDeadLineDate] = useState("");
+  const [deadLineTime, setDeadLineTime] = useState("");
   const [tasks, setTasks] = useState([]);
   const [isDisplayed, setIsDisplayed] = useState(false);
 
+  const setDeadLineDateHandler = (date) => {
+    setDeadLineDate(date);
+  };
+  const setDeadLineTimeHandler = (time) => {
+    setDeadLineTime(time);
+  };
+  const timeContext = {
+    deadLineDate,
+    setDeadLineDate: setDeadLineDateHandler,
+    deadLineTime,
+    setDeadLineTime: setDeadLineTimeHandler,
+  };
   const handleDeleteTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
@@ -18,35 +34,49 @@ function App() {
     setIsDisplayed((prevState) => !prevState);
   };
   return (
-    <div className=" flex ">
-      <div className=" min-w-fit p-5 h-fit m-2 ml-0 bg-zinc-100 rounded-lg flex flex-col items-start ">
-        <ButtonComponent
-          onClick={handleToggle}
-          context="کار جدید"
-          icon="add"
-          className="sideBar"
-        />
-        <ButtonComponent context="پیش نویس" icon="draft" className="sideBar" />
-        <ButtonComponent
-          context="کارهای امروز"
-          icon="today"
-          className="sideBar"
-        />
-        <ButtonComponent context="همه کارها" icon="week" className="sideBar" />
-        <hr class="border-t-2 border-gray-300 w-full max-w-md my-4" />
-        <ButtonComponent context="پروژه های من" className="sideBar" />
-        <ButtonComponent context="شخصی" icon="personal" className="sideBar" />
-        <ButtonComponent context="خانه" icon="home" className="sideBar" />
-        <ButtonComponent context="شغلی" icon="business" className="sideBar" />
-        <ButtonComponent context="ورزش" icon="sport" className="sideBar" />
-        <ButtonComponent context="مطالعه" icon="study" className="sideBar" />
-        <ButtonComponent context="مناسبت" icon="birthday" className="sideBar" />
+    <TimeContext.Provider value={timeContext}>
+      <div className=" flex ">
+        <div className=" min-w-fit p-5 h-fit m-2 ml-0 bg-zinc-100 rounded-lg flex flex-col items-start ">
+          <ButtonComponent
+            onClick={handleToggle}
+            context="کار جدید"
+            icon="add"
+            className="sideBar"
+          />
+          <ButtonComponent
+            context="پیش نویس"
+            icon="draft"
+            className="sideBar"
+          />
+          <ButtonComponent
+            context="کارهای امروز"
+            icon="today"
+            className="sideBar"
+          />
+          <ButtonComponent
+            context="همه کارها"
+            icon="week"
+            className="sideBar"
+          />
+          <hr class="border-t-2 border-gray-300 w-full max-w-md my-4" />
+          <ButtonComponent context="پروژه های من" className="sideBar" />
+          <ButtonComponent context="شخصی" icon="personal" className="sideBar" />
+          <ButtonComponent context="خانه" icon="home" className="sideBar" />
+          <ButtonComponent context="شغلی" icon="business" className="sideBar" />
+          <ButtonComponent context="ورزش" icon="sport" className="sideBar" />
+          <ButtonComponent context="مطالعه" icon="study" className="sideBar" />
+          <ButtonComponent
+            context="مناسبت"
+            icon="birthday"
+            className="sideBar"
+          />
+        </div>
+        <TaskListComponent tasks={tasks} deleteTask={handleDeleteTask} />
+        <div className={isDisplayed ? "" : " hidden"}>
+          <AddFormComponent onAddTask={handleAddTask} onExit={handleToggle} />
+        </div>
       </div>
-      <TaskListComponent tasks={tasks} deleteTask={handleDeleteTask} />
-      <div className={isDisplayed ? "" : " hidden"}>
-        <AddFormComponent onAddTask={handleAddTask} onExit={handleToggle} />
-      </div>
-    </div>
+    </TimeContext.Provider>
   );
 }
 
